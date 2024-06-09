baefe3203a7f3617fa3dc203878f54636a76c019
carlosbne <106997150+carlosbne@users.noreply.github.com>
2024-06-09 00:17:24 -0300

    some homework

--- class19-materials/arrays-fight-to-the-finish/js/main.js
+++ class19-materials/arrays-fight-to-the-finish/js/main.js
@@ -1,7 +1,27 @@
 //Create an array of movie titles. Loop through the array and each element to the h2.
+let movies = ['the shining', 'Shrek', 'About Time']
+let getH2 = document.querySelector('h2');
+
+for(let i = 0; i < movies.length; i++){
+    getH2.innerText += movies[i];
+}
+
+//Create an array of numbers. Loop through the array and add three to each number and replace the old number.
+const nums = [10, 20, 30, 40, 50, 60, 70];
 
 
-//Create an array of numbers. Loop through the array and three to each number and replace the old number.
-
+function troca(x){
+    let cont = 0;
+    for(let i = 0; i < x.length; i++){
+        x[i] = x[i] + 3;
+        if(cont === 3){
+            x[i] = x[i-1];
+            cont = 0;
+        }
+        cont++;
+    }
+    return x;
+}
 
 //Find the average of all the numbers from question three
+let conta = 0;
--- class19-materials/arrays-in-it-to-win-it/js/main.js
+++ class19-materials/arrays-in-it-to-win-it/js/main.js
@@ -6,3 +6,16 @@ let firstMovie = movies[0];
 let lenArr = movies.length;
 //Get the last element in that array and store it in a new variable. What if your array was really large and you didn't know the last index? Would your solution still work?
 let lastMovie = movies[movies.length - 1];
+
+/* 
+let bestColors = ['green','blue','yellow','black']
+
+for(let i = 0; i < bestColors.length;i++){
+  console.log( bestColors[i] )
+}
+ */
+//forEach - method for arrays
+let bestColors = ['green','blue','yellow','black']
+
+bestColors.forEach((x,i)=> console.log(x))
+
