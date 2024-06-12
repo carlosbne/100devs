ba71f0edb76eea081fa240b8b95cc56f391636d6
carlosbne <106997150+carlosbne@users.noreply.github.com>
2024-06-12 14:57:57 -0300

    new homework

--- class20-materials/review-summer-games/js/main.js
+++ class20-materials/review-summer-games/js/main.js
@@ -1,1 +1,14 @@
 //Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+
+function mult(arr){
+   alert(arr.reduce((x, i) => x * i, 1));
+}
+
+
+// did by leon
+function multiNumber(arr){
+    let product = 1;
+    arr.forEach(num => product *= num);
+
+    alert(product);
+}
--- class20-materials/review-winter-games/js/main.js
+++ class20-materials/review-winter-games/js/main.js
@@ -1,1 +1,14 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+
+function justEvens(arr){
+    let newArr = [];
+    arr.forEach((item) => item % 2 === 0 ? newArr.push(item) : '' );
+
+    return newArr;
+}
+
+function justPares(arr){
+    let newArr = arr.map((item) => item % 2 === 0);
+
+    console.log(newArr);
+}
