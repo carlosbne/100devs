59cc396c0836df05abbf0a0efecbb456f09da8d7
carlosbne <106997150+carlosbne@users.noreply.github.com>
2024-06-18 23:08:01 -0300

    somthing with objects

--- class21-materials/objects-zeo/js/main.js
+++ class21-materials/objects-zeo/js/main.js
@@ -1,1 +1,25 @@
 //Create a stopwatch object that has four properties and three methods
+let stopwatch = {}
+
+stopwatch.color = 'black';
+stopwatch.currentTime = '29:12';
+stopwatch.brand = 'olympikus';
+stopwatch.shape = 'round';
+
+stopwatch.tellTimer = function (time){
+    console.log(`The current time is ${time}`);
+}
+
+stopwatch.resetTimer = function (time){
+    stopwatch.currentTime = '00:00';
+    console.log(`The current Time is ${stopwatch.currentTime}`);
+}
+
+stopwatch.changeColor = function(color){
+    stopwatch.color = color;
+    console.log(`The new color is ${stopwatch.color}`);
+}
+
+stopwatch.tellTimer(stopwatch.currentTime);
+stopwatch.resetTimer(stopwatch.currentTime);
+stopwatch.changeColor('orange');
