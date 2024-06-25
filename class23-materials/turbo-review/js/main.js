905a2e1ee04ab5598e917bf363a4e32dc0d7d9d8
carlosbne <106997150+carlosbne@users.noreply.github.com>
2024-06-24 22:47:36 -0300

    new class, new atv

---
+++ class23-materials/turbo-review/js/main.js
@@ -0,0 +1,15 @@
+// *Variables*
+// Declare a variable and assign it to a sentance as a string. Alert if the sentance is a question
+
+//Declare a variable, assign it a string of multiple words, replace every "jr. dev" with "software engineer", and print it to the console
+let words = "Hello jr. dev here jr. dev are jr. dev one jr. dev"
+let newWords = words.replaceAll('jr. dev', 'software engineer');
+console.log(newWords);
+// *Functions*
+// Create a function that returns rock, paper, or scissors as randomly as possible
+
+// *Conditionals*
+//Create a function that takes in a choice (rock, paper, or scissors) and determines if they won a game of rock paper scissors against a bot using the above function
+
+//*Loops*
+//Create a function that takes an array of choices. Play the game x times where x is the number of choices in the array. Print the results of each game to the console.
