9e4e7fdec65ca7f53d96b1ccedd248c7cdd33ba6
carlosbne <106997150+carlosbne@users.noreply.github.com>
2024-06-25 13:36:47 -0300

    some atv

--- class23-materials/space-review/js/main.js
+++ class23-materials/space-review/js/main.js
@@ -1,12 +1,45 @@
 //Arrays
 
 //Create and array of numbers. Sum all of the numbers. Alert the sum.
+let arr = [1, 2, 3, 4, 5, 6];
+
+let sumx = arr.reduce((acc, c) => acc + c, 0);
+
+alert(sumx);
 
 //Create a function that takes in an array of numbers
 //Return a new array of numbers that is every original number squared
+function squared(array){
+    let newArr = array.map((element) => Math.pow(element, element));
+    return newArr;
+}   
+
+console.log(squared(arr));
 
 //Create a function that takes string
 //Print the reverse of that string to the console
 
+function reversed(str){
+    return str.split('').reverse().join('');
+}
+
+//-----recursion---------
+/* function reversed(str){
+    if(str === '')
+        return '';
+    else{
+        return reversed(str.substring(1)) + str.charAt(0);
+    }
+} */
+
+
+
 //Create a function that takes in a string
 //Alert if the string is a palindrome or not
+
+function isPalindrome(str){
+    let reversedStri = reversed(str);
+    return reversedStri === str ? alert(`Is a palindrome`) : alert(`is not`);
+}
+
+console.log(isPalindrome('tenet'));
--- class23-materials/turbo-review/js/main.js
+++ class23-materials/turbo-review/js/main.js
@@ -1,5 +1,10 @@
 // *Variables*
 // Declare a variable and assign it to a sentance as a string. Alert if the sentance is a question
+let sentance = 'how are you doing?';
+
+if(sentance.endsWith('?')){
+    alert(sentance);
+}
 
 //Declare a variable, assign it a string of multiple words, replace every "jr. dev" with "software engineer", and print it to the console
 let words = "Hello jr. dev here jr. dev are jr. dev one jr. dev"
@@ -7,9 +12,33 @@ let newWords = words.replaceAll('jr. dev', 'software engineer');
 console.log(newWords);
 // *Functions*
 // Create a function that returns rock, paper, or scissors as randomly as possible
-
+function rockPaperScissor(){
+    let random = Math.floor(Math.random() * 3);
+     
+    if(random === 0){
+        return 'rock';
+    }else if (random === 1){
+        return 'paper';
+    }else{
+        return 'scissors';
+    } 
+}
 // *Conditionals*
 //Create a function that takes in a choice (rock, paper, or scissors) and determines if they won a game of rock paper scissors against a bot using the above function
-
+function checkWin(choice){
+    let bot = rockPaperScissor();
+    if(choice === 'rock' && bot === 'scissors' || choice === 'paper' && bot === 'rock' || choice === 'scissors' && bot === 'paper'){
+        return `You winnn!`;
+    }else if(choice === bot){
+        return `a tie`;
+    }else{
+        return `You lose`;
+    }
+}
 //*Loops*
 //Create a function that takes an array of choices. Play the game x times where x is the number of choices in the array. Print the results of each game to the console.
+function resultOfChoices(choices){
+    for(let i = 0; i < choices.length; i++){
+        console.log(checkWin(choices[i]));
+    }
+}
